d19ce3ed2c8b463a90a85fa22599ac13b146ae48
cnaldaba <cnaldaba@gmail.com>
2014-11-08 16:26:48 -0600

    Added Data Array

---
+++ src/com/example/blesensortag/dataArray.java
@@ -0,0 +1,13 @@
+package com.example.blesensortag;
+
+public class dataArray {
+	public double xaxis;
+	public double yaxis;
+	public double zaxis;
+	
+	 public dataArray(double Xvalue, double Yvalue, double Zvalue) {
+	    	xaxis = Xvalue;
+	    	yaxis = Yvalue;
+	    	zaxis = Zvalue;
+	 }
+}
